bf01996e29a3ff879be31b7a258d18fdfe12713f
Oliver Walczak <olly.walczak@gmx.de>
2025-04-08 22:07:05 +0200

    Add AdcVis example (working)

--- test/AdcVis/Core/Src/main.c
+++ test/AdcVis/Core/Src/main.c
@@ -18,10 +18,11 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "usb_device.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "usbd_cdc_if.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -31,7 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define USBBUF_MAXLEN 128
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -40,7 +41,6 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc1;
 
 /* USER CODE BEGIN PV */
 
@@ -56,7 +56,21 @@ static void MX_ADC1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+uint8_t usbTxBuf[USBBUF_MAXLEN];
+uint8_t usbRxBuf[USBBUF_MAXLEN];
 
+uint16_t usbTxBufLen = 0;
+uint16_t usbRxBufLen = 0;
+
+uint8_t  usbRxFlag 	 = 0;
+
+void USB_RXCallback(uint8_t* Buf, uint32_t *Len) {
+
+	memcpy(usbRxBuf, Buf, *Len);
+	usbRxBufLen = *Len;
+	usbRxFlag = 1;
+
+}
 /* USER CODE END 0 */
 
 /**
@@ -89,17 +103,71 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
+  // Follow https://youtu.be/kDaQO_x8auk?si=l_U3NRmKQmfKEMqJ for USB CDC setup
+
+  LL_ADC_Enable(ADC1);
+  uint32_t usbTxTime = 0;
+
   while (1)
   {
+	  uint32_t timeNowMS = HAL_GetTick();
+
+	  LL_ADC_INJ_StartConversionSWStart(ADC1);
+	  uint16_t adcValue = LL_ADC_INJ_ReadConversionData12(ADC1, LL_ADC_INJ_RANK_1);
+
+	  // 0 <= adcValue <= 4095 -> 16 leds
+	  double prod = adcValue * 15.0;
+	  uint8_t led = (int)(prod / 4095 + 0.5);
+
+	  // Reset GPIOB
+	  GPIOB->BSRR = 0xffff << 16;
+      // Set pin according to led
+	  GPIOB->BSRR = (1 << led);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  if(timeNowMS - usbTxTime > 500) {
+		  usbTxTime = timeNowMS;
+
+		  usbTxBufLen = snprintf((char *) usbTxBuf, USBBUF_MAXLEN, "[%02lu:%02lu] %u\r\n", (timeNowMS / 60000), (timeNowMS / 1000) % 60, adcValue);
+
+		  CDC_Transmit_FS(usbTxBuf, usbTxBufLen);
+
+	  }
+
+	  if (usbRxFlag && usbRxBufLen) {
+
+		  // Echo incoming data
+		  CDC_Transmit_FS(usbRxBuf, usbRxBufLen);
+
+		  for(uint8_t i = 0; i < usbRxBufLen; ++i) {
+
+			  // Reset GPIOB
+			  GPIOB->BSRR = 0xffff << 16;
+		      // Set all pins
+			  GPIOB->BSRR = 0xffff;
+
+			  LL_mDelay(100);
+
+			  // Reset GPIOB
+			  GPIOB->BSRR = 0xffff << 16;
+
+			  LL_mDelay(100);
+		  }
+
+		  usbRxFlag = 0;
+		  usbRxBufLen = 0;
+
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -123,6 +191,7 @@ void SystemClock_Config(void)
 
   }
   LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 168, LL_RCC_PLLP_DIV_2);
+  LL_RCC_PLL_ConfigDomain_48M(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 168, LL_RCC_PLLQ_DIV_7);
   LL_RCC_PLL_Enable();
 
    /* Wait till PLL is ready */
@@ -164,40 +233,49 @@ static void MX_ADC1_Init(void)
 
   /* USER CODE END ADC1_Init 0 */
 
-  ADC_ChannelConfTypeDef sConfig = {0};
+  LL_ADC_InitTypeDef ADC_InitStruct = {0};
+  LL_ADC_REG_InitTypeDef ADC_REG_InitStruct = {0};
+  LL_ADC_CommonInitTypeDef ADC_CommonInitStruct = {0};
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* Peripheral clock enable */
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_ADC1);
+
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  /**ADC1 GPIO Configuration
+  PA0-WKUP   ------> ADC1_IN0
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_0;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN ADC1_Init 1 */
 
   /* USER CODE END ADC1_Init 1 */
 
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  /** Common config
   */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  ADC_InitStruct.Resolution = LL_ADC_RESOLUTION_12B;
+  ADC_InitStruct.DataAlignment = LL_ADC_DATA_ALIGN_RIGHT;
+  ADC_InitStruct.SequencersScanMode = LL_ADC_SEQ_SCAN_DISABLE;
+  LL_ADC_Init(ADC1, &ADC_InitStruct);
+  ADC_REG_InitStruct.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
+  ADC_REG_InitStruct.SequencerLength = LL_ADC_REG_SEQ_SCAN_DISABLE;
+  ADC_REG_InitStruct.SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE;
+  ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_SINGLE;
+  ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_NONE;
+  LL_ADC_REG_Init(ADC1, &ADC_REG_InitStruct);
+  LL_ADC_REG_SetFlagEndOfConversion(ADC1, LL_ADC_REG_FLAG_EOC_UNITARY_CONV);
+  ADC_CommonInitStruct.CommonClock = LL_ADC_CLOCK_SYNC_PCLK_DIV4;
+  ADC_CommonInitStruct.Multimode = LL_ADC_MULTI_INDEPENDENT;
+  LL_ADC_CommonInit(__LL_ADC_COMMON_INSTANCE(ADC1), &ADC_CommonInitStruct);
 
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_0;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_0);
+  LL_ADC_SetChannelSamplingTime(ADC1, LL_ADC_CHANNEL_0, LL_ADC_SAMPLINGTIME_480CYCLES);
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
